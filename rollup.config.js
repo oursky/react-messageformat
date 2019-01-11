8555b2e63f4f5c6c2dab91aa8d6f46218d4248a4
Louis Chan <louischan@oursky.com>
2019-01-11 15:18:29 +0800

    Separate production bundle and development bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,26 @@
 import * as ts from "typescript";
-import typescript from "rollup-plugin-typescript";
 import commonjs from "rollup-plugin-commonjs";
 import json from "rollup-plugin-json";
+import replace from "rollup-plugin-replace";
 import resolve from "rollup-plugin-node-resolve";
+import typescript from "rollup-plugin-typescript";
 import pkg from "./package.json";
 
 const deps = Object.keys(pkg.dependencies || {});
 const peerDeps = Object.keys(pkg.peerDependencies || {});
 const allDeps = deps.concat(peerDeps);
+
+function external(id) {
+  if (id === "tslib") {
+    return false;
+  }
+  for (const d of allDeps) {
+    if (id.startsWith(d)) {
+      return true;
+    }
+  }
+  return false;
+}
 
 const extensions = [".mjs", ".js", ".jsx", ".ts", ".tsx"];
 
@@ -27,34 +40,24 @@ const plugins = [
   }),
 ];
 
-const output = [
-  {
-    format: "cjs",
-    file: "dist/index.cjs.js",
-    interop: false,
-  },
-  {
-    format: "esm",
-    file: "dist/index.esm.js",
-    interop: false,
-  },
-];
-
-function external(id) {
-  if (id === "tslib") {
-    return false;
-  }
-  for (const d of allDeps) {
-    if (id.startsWith(d)) {
-      return true;
-    }
-  }
-  return false;
+function makeConfig(env) {
+  return {
+    input: "src/index.ts",
+    external,
+    plugins: [
+      replace({
+        values: {
+          "process.env.NODE_ENV": JSON.stringify(env),
+        },
+      }),
+      ...plugins,
+    ],
+    output: {
+      format: "cjs",
+      file: `dist/index.${env}.cjs.js`,
+      interop: true,
+    },
+  };
 }
 
-export default {
-  input: "src/index.ts",
-  external,
-  plugins,
-  output,
-};
+export default [makeConfig("development"), makeConfig("production")];
